824fcb0b1021b9839683165ed4f263fcb0e4332a
atman1991 <atman1991@naver.com>
2020-01-04 01:04:11 +0900

    commit

---
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ProfileController {
+    @Autowired
+    private Environment env;
+
+    public ProfileController() {}
+
+    public ProfileController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
